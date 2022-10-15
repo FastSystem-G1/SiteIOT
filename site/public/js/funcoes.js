ec58d56be008c81f08a3212f3a5bba2938b5e64e
FelipeGomes-20 <felipe.gomes@sptech.school>
2022-10-15 00:21:28 -0300

    Telas padronizadas

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -4,14 +4,20 @@ function validarSessao() {
 
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
+    var cargo = sessionStorage.CARGO;
 
-   
-
-if (email != null && nome != null)  {
+if (email != null && nome != null && cargo == 1)  {
     
        /*  document.getElementsByClassName('nome_usuario')[0].innerHTML = sessionStorage.NOME_USUARIO; */
 
         // finalizarAguardar();
+    }else if(email != null && nome != null & cargo == 0){
+        document.getElementById('nav-list').innerHTML =
+       ` <ul class="nav-list">
+            <li><a href="../Dashboard/dashInicial.html">Início</a></li>
+            <li onclick="sair()"><a href="../login.html">Sair</a></li>
+            <li></li>
+        </ul> `
     } else {
         window.location = "login.html";
     }
