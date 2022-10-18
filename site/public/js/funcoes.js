fc3c8f0bd27eef3f8a9af35f386aaa1d87888159
Felipe <felipe.gomes@sptech.school>
2022-10-18 02:44:16 -0300

    Validações nas telas funcionando

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -6,36 +6,98 @@ function validarSessao() {
     var nome = sessionStorage.NOME_USUARIO;
     var cargo = sessionStorage.CARGO;
 
-if (email != null && nome != null && cargo == true)  {
-      // window.location = "./Dashboard/dashInicial";
+    if (email =! null && nome != null && cargo == "true")  {
+        document.getElementById('nav-content').innerHTML = 
+        `<div style="display: flex; justify-content: center; align-items: center; flex-direction: column; justify-content: space-around; align-items: center;
+        letter-spacing: 3px;
+        margin: 60px 0;  font-size: 25px;">
+        <div style="display: flex; justify-content: center; align-items: center; flex-direction: column; justify-content: space-around; align-items: center;
+        letter-spacing: 3px;
+        margin: 60px 0; color: #00C2CB; font-size: 30px; font-weight: bold;">Bem vindo(a), ${nome}!</div>
 
-       document.getElementById('texto_func').innerHTML =
-       `
-        Olá, ${nome} ! </br>
-        Empresa: ${sessionStorage.EMPRESA}
-       `;
+        <a href= "dashInicial.html" style = "margin-bottom: 30px;">Início</a>
+        <a href="../Telas de Cadastro/cadastroMaquina.html" style = "margin-bottom: 30px;">Cadastrar máquina</a>
+        <a href="../Telas de Cadastro/cadastroAplicativo.html" style = "margin-bottom: 30px;">Cadastrar aplicativo</a>
+        <a href="../Telas de Cadastro/NovoFuncionario.html" style = "margin-bottom: 30px;">Cadastrar funcionário</a>
+        <a href="../login.html" onclick="sair()">Sair</a>
 
-    }else if(email != null && nome != null && cargo == false){
 
-        document.getElementById('texto_func').innerHTML =
-        `
-         Olá, ${nome} ! </br>
-         Empresa: ${sessionStorage.EMPRESA}
+        </div>
         `;
+      
+
+    }else if(email =! null && nome != null && cargo == "false"){
 
         document.getElementById('nav-content').innerHTML = 
         `<div style="display: flex; justify-content: center; align-items: center; flex-direction: column; justify-content: space-around; align-items: center;
         letter-spacing: 3px;
         margin: 60px 0;">
+        <div style="display: flex; justify-content: center; align-items: center; flex-direction: column; justify-content: space-around; align-items: center;
+        letter-spacing: 3px;
+        margin: 60px 0; color: #00C2CB; font-size: 30px; font-weight: bold;">Bem vindo(a), ${nome}!</div>
 
         <a href= "dashInicial.html" style = "margin-bottom: 60px; font-size: 25px;">Início</a>
-        <a href="../login.html" style = "margin-bottom: 60px; font-size: 25px;">Sair</a>
+        <a href="../login.html" onclick="sair()">Sair</a>
 
 
         </div>
         `;
+
+        
     } else {
-       // window.location = "../login.html";
+       window.location = "../login.html";
+
+    }
+}
+
+function validarSessao2() {
+    // aguardar();
+
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+    var cargo = sessionStorage.CARGO;
+
+    if (email =! null && nome != null && cargo == "true")  {
+        document.getElementById('nav-content').innerHTML = 
+        `<div style="display: flex; justify-content: center; align-items: center; flex-direction: column; justify-content: space-around; align-items: center;
+        letter-spacing: 3px;
+        margin: 60px 0;  font-size: 25px;">
+        <div style="display: flex; justify-content: center; align-items: center; flex-direction: column; justify-content: space-around; align-items: center;
+        letter-spacing: 3px;
+        margin: 60px 0; color: #00C2CB; font-size: 30px; font-weight: bold;">Bem vindo(a), ${nome}!</div>
+
+        <a href= "../Dashboard/dashInicial.html" style = "margin-bottom: 30px;">Início</a>
+        <a href="../Telas de Cadastro/cadastroMaquina.html" style = "margin-bottom: 30px;">Cadastrar máquina</a>
+        <a href="../Telas de Cadastro/cadastroAplicativo.html" style = "margin-bottom: 30px;">Cadastrar aplicativo</a>
+        <a href="../Telas de Cadastro/NovoFuncionario.html" style = "margin-bottom: 30px;">Cadastrar funcionário</a>
+        <a href="../login.html" onclick="sair()">Sair</a>
+
+
+        </div>
+        `;
+      
+
+    }else if(email =! null && nome != null && cargo == "false"){
+
+        document.getElementById('nav-content').innerHTML = 
+        `<div style="display: flex; justify-content: center; align-items: center; flex-direction: column; justify-content: space-around; align-items: center;
+        letter-spacing: 3px;
+        margin: 60px 0;">
+        <div style="display: flex; justify-content: center; align-items: center; flex-direction: column; justify-content: space-around; align-items: center;
+        letter-spacing: 3px;
+        margin: 60px 0; color: #00C2CB; font-size: 30px; font-weight: bold;">Bem vindo(a), ${nome}!</div>
+
+        <a href= "../Dashboard/dashInicial.html" style = "margin-bottom: 60px; font-size: 25px;">Início</a>
+        <a href="../login.html" onclick="sair()">Sair</a>
+
+
+        </div>
+        `;
+
+        
+    } else {
+       window.location = "../login.html";
+
     }
 }
 
