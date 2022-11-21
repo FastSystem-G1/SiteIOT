b4b95bdb4ff4847623471146ac7093f68dc57275
EndrylFiorotti <endryl.fiorotti@sptech.school>
2022-11-20 21:09:20 -0300

    Estilização das dashs

--- site/src/controllers/maquinaController.js
+++ site/src/controllers/maquinaController.js
@@ -33,25 +33,7 @@ function pegarMaquinas(req, res) {
     });
 }
 
-function pegarMaquinas2(req, res) {
-    console.log(`Recuperando Máquinas.`);
-    var componente = req.params.nome_componente;
-    var medida = req.params.medida;
-    maquinaModel.pegarMaquinas2(componente, medida).then(function (resultado) {
-        if (resultado.length > 0) {
-            res.status(200).json(resultado);
-        } else {
-            res.status(204).send("Nenhum resultado encontrado!")
-        }
-    }).catch(function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
-    });
-}
-
 module.exports = {
     listarMaquina,
     pegarMaquinas,
-    pegarMaquinas2,
 }
